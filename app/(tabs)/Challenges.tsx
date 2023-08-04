06014aaad8e10e87444b427b9be37fff263618b8
Allen Odoom <allencvb@gmail.com>
2023-08-03 21:35:46 -0400

    Tabs

--- app/(tabs)/Challenges.tsx
+++ app/(tabs)/Challenges.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
@@ -18,3 +19,62 @@ const styles = StyleSheet.create({
 });
 
 export default Challenges;
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import React from 'react';
+import { View, Text, ImageBackground, StyleSheet, TouchableOpacity } from 'react-native';
+//import backgroundImage from '../../assets/fonts/'; // Adjust the path as needed
+
+const MyComponent = () => {
+  return (
+    <ImageBackground source={backgroundImage} style={styles.background}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Welcome to My App</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Click Me</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover', // or 'stretch'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: 'white',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: 'white',
+  },
+});
+
+export default MyComponent;
+
+*/
